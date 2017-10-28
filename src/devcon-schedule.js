98ded9b3e70f39813dbad335f45f4e36d8ba47c4
David Schmitz <david.schmitz.privat@gmail.com>
2017-10-28 09:19:10 +0200

    Added tests for devcon schedule

--- src/devcon-schedule.js
+++ src/devcon-schedule.js
@@ -1,6 +1,4 @@
 const sortTalks = function (talks) {
-    const now = new Date();
-
     return talks.sort(
                 (talkA, talkB) => {
                     const aStart = Date.parse(talkA.startsAt);
@@ -10,19 +8,14 @@ const sortTalks = function (talks) {
 };
 
 const findNextTalk = function (now, sortedTalks) {
-    console.log('Find next', sortedTalks);
-    return sortedTalks.find(talk => Date.parse(talk.startsAt) > now.getTime());
+    return sortedTalks.find(talk => Date.parse(talk.startsAt) > now);
 };
 
 module.exports = {
     getNextTalk(currentTime, schedule) {
         return schedule
-            .then(s =>  s[0]);
-             //findNextTalk(new Date(), sortTalks(devcon.schedule)));
-    },
-    getTalkSummary(currentTime) {
-        return {
-
-        };
+            .then(s =>  {
+                return findNextTalk(currentTime, sortTalks(s));
+            });
     }
 };
--- test/devcon-schedule-test.js
+++ test/devcon-schedule-test.js
@@ -5,26 +5,28 @@ chai.should();
 const expect = chai.expect;
 
 const DevconSchedule = require('../src/devcon-schedule');
+const testSchedule = require('./test-schedule');
 
 lab.experiment('the schedule', () => {
-    lab.beforeEach((done) => {
-        done();
+    lab.test('should return the next talk after the current timestamp', (done) => {
+        const now = Date.parse('2017-11-10T10:00:10.262Z');
+
+        const talk = DevconSchedule.getNextTalk(now, Promise.resolve(testSchedule.schedule));
+        return talk.then(t => expect(t.talkId).to.be.equal('3'));
     });
 
-    lab.afterEach((done) => {
-        done();
+    lab.test('should return undefined if schedule is empty', (done) => {
+        const now = Date.parse('2017-11-10T10:00:10.262Z');
+
+        const talk = DevconSchedule.getNextTalk(now, Promise.resolve([]));
+        return talk.then(t => expect(t).to.be.undefined);
     });
 
-    lab.test('should ', (done) => {
-        done();
-    });
+    lab.test('should return undefined if no talk is found', (done) => {
+        const now = Date.parse('2017-11-11T10:00:10.262Z');
 
-    lab.test('should ', (done) => {
-        done();
-    });
-
-    lab.test('should ', (done) => {
-        done();
+        const talk = DevconSchedule.getNextTalk(now, Promise.resolve(testSchedule.schedule.slice(0, 1)));
+        return talk.then(t => expect(t).to.be.undefined);
     });
 
     lab.test('should ', (done) => {
