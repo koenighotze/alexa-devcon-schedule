d1b166340f3ffc15fc57f06ce0b88d7d5a1404fb
David Schmitz <david.schmitz.privat@gmail.com>
2017-11-07 12:55:47 +0100

    Added whitecard cors headers to lambda

--- src/schedule.js
+++ src/schedule.js
@@ -3,7 +3,14 @@ const schedule = require('./s3-schedule').schedule;
 
 exports.handler = (event, context, callback) => {
   console.log('Try to fetch the schedule');
-  schedule.then(talks => callback(null, { body: JSON.stringify({ talks: talks }) }))
+  schedule.then(talks => callback(null,
+      {
+        headers: {
+          "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
+          "Access-Control-Allow-Credentials" : true // Required for cookies, authorization headers with HTTPS
+        },
+        body: JSON.stringify({ talks: talks })
+      }))
           .catch(err => {
             console.log('Fetching the schedule failed', err);
             callback("Cannot fetch schedule.");
